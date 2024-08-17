af4f94a25599b12509d339bba13f9dd5e0f20e72
JUPE <jupe@odoo.com>
2024-08-17 14:34:01 +0200

    first step creation estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Estate Module",
+    'depends': ['base'],
+    'author': "JUPE (based on Odoo doc)",
+    'category': 'Category',
+    # # data files always loaded at installation
+    # 'data': [
+    #     'views/mymodule_view.xml',
+    # ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
